286cabd2e74f78ab793d4c0def15a334d0aba39c
Sam Calisch <s.calisch@gmail.com>
2016-03-21 17:37:06 -0400

    added new recipes for avr-gcc v5.3 and avr-libc v2.0

---
+++ avr-gcc53.rb
@@ -0,0 +1,62 @@
+require 'formula'
+
+# print avr-gcc's builtin include paths
+# `avr-gcc -print-prog-name=cc1plus` -v
+
+class AvrGcc < Formula
+
+    homepage 'http://www.gnu.org/software/gcc/gcc.html'
+    url 'ftp://gcc.gnu.org/pub/gcc/releases/gcc-5.3.0/gcc-5.3.0.tar.bz2'
+    mirror 'http://ftpmirror.gnu.org/gcc/gcc-5.3.0/gcc-5.3.0.tar.bz2'
+    sha256 '2332b2a5a321b57508b9031354a8503af6fdfb868b8c1748d33028d100a8b67e'
+
+    depends_on 'gmp'
+    depends_on 'libmpc'
+    depends_on 'mpfr'
+
+    depends_on 'avr-binutils'
+
+    option 'disable-cxx', 'Don\'t build the g++ compiler'
+
+    def install
+        # The C compiler is always built, C++ can be disabled
+        languages = %w[c]
+        languages << 'c++' unless build.include? 'disable-cxx'
+
+        args = [
+            "--target=avr",
+            "--prefix=#{prefix}",
+
+            "--enable-languages=#{languages.join(',')}",
+            "--with-gnu-as",
+            "--with-gnu-ld",
+            "--with-ld=#{Formula["avr-binutils"].opt_bin/'avr-ld'}",
+            "--with-as=#{Formula["avr-binutils"].opt_bin/'avr-as'}",
+
+            "--disable-nls",
+            "--disable-shared",
+            "--disable-threads",
+            "--disable-libssp",
+            "--disable-libstdcxx-pch",
+            "--disable-libgomp",
+
+            "--with-gmp=#{Formula["gmp"].opt_prefix}",
+            "--with-mpfr=#{Formula["mpfr"].opt_prefix}",
+            "--with-mpc=#{Formula["libmpc"].opt_prefix}",
+            "--with-system-zlib"
+        ]
+
+        mkdir 'build' do
+            system "../configure", *args
+            system "make"
+
+            ENV.deparallelize
+            system "make install"
+        end
+
+        # info and man7 files conflict with native gcc
+        info.rmtree
+        man7.rmtree
+    end
+end
+
